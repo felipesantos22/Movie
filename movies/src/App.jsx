2a17b6d5891107425c3a3c95b2c131f5e11d71b2
felipesantos22 <felipevs2018@gmail.com>
2022-11-30 14:46:40 -0300

    routes

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,10 +1,19 @@
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {  
 
   return (
     <div className="App">
+      <nav>
+        <h2>
+          <Link to={'/'}>MoviesLib</Link>
+        </h2>
+        <Link to={'/movie/1'}>Movie</Link>
+        <Link to={'/search'}>Search</Link>
+      </nav>
      <h1>Movie</h1>
+     <Outlet/>
     </div>
   )
 }
